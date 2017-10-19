12502488f83d9af09f90ccf772aee82f79ddc046
Tiago Coelho <tiagocoelho33@gmail.com>
2017-10-19 21:34:26 -0200

    execution in periodic time

--- Server/api/scrape.js
+++ Server/api/scrape.js
@@ -2,6 +2,54 @@ const request = require('request');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
+//Comeca buscando os 10 primeiros deputados
+const incresseSearch = 10;
+
+var indexSearchBegin = 1;
+var indexSearchEnd = indexSearchBegin+incresseSearch;
+
+var resetaIndex = false;
+
+/**
+ * Excuta o @metodo getDeputado a cada 10 segundos\
+ */
+setInterval(getDeputado, 10*1000); //time is in ms
+function getDeputado(){
+    console.log("Info status running: indexBegin = "+indexSearchBegin+" ## indexEnd = "+indexSearchEnd);
+
+    request("http://www2.camara.leg.br/deputados/pesquisa", function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const dom = new JSDOM(body);
+            var arraySize = dom.window.document.getElementById('deputado').length;
+            for (var i = indexSearchBegin; i < indexSearchEnd; i++) {
+                //Seleciona a comboBox com a lista de deputados
+                var temp = dom.window.document.getElementById('deputado')[i].value;
+                //Pesquisa o deputdo pela sua id
+                parseDeputadoInfo(temp.split("?")[1])
+            }
+            //Controla pra ver se o array ja chegou ate o final
+            if(!resetaIndex){
+                indexSearchBegin += incresseSearch;
+                indexSearchEnd += incresseSearch;
+                // Se o index for maior que o tamanho do array, significa que chegou no final.
+                // Vai executar mais uma vez e resetar os contadores.
+                if(indexSearchEnd > arraySize){
+                    indexSearchEnd = arraySize;
+                    resetaIndex = true;
+                }
+            }else{
+                indexSearchBegin = 1;
+                indexSearchEnd = indexSearchBegin+incresseSearch;
+                resetaIndex = false;
+            }
+        }
+    });
+}
+
+/**
+ * Pesquisa o deputado pela id e monta um objeto JSON com as informacao obtidas na pagina HTML
+ * @param {Number} deputadoId 
+ */
 function parseDeputadoInfo(deputadoId) {
     var options = {
         url: 'http://www.camara.leg.br/internet/Deputado/dep_Detalhe.asp?id=' + deputadoId,
@@ -13,6 +61,7 @@ function parseDeputadoInfo(deputadoId) {
         if (!error && response.statusCode == 200) {
             const dom = new JSDOM(body);
             var pageSections = dom.window.document.getElementsByClassName('visualNoMarker');
+            //JSON exemplo
             var newDeputado = {
                 "fullname": '',
                 "bday": '',
@@ -28,8 +77,8 @@ function parseDeputadoInfo(deputadoId) {
                 },
                 "mainCommission": '',
                 "substituteCommission": '',
-
             }
+
             var infoDep = pageSections[0];
             /** O array possui 7 elementos porem so precismos dos 5 primeiros
              * Linha 1 - nome
@@ -97,6 +146,10 @@ function parseDeputadoInfo(deputadoId) {
     });
 }
 
+/**
+ * Recebe um obj JSON e faz uma chamada para a API do server para salvar o obj no mongodb
+ * @param {JSONObject} newDeputado 
+ */
 function salvaDeputado(newDeputado) {
     var options = {
         url: 'http://localhost:3000/api/v0/deputados',
@@ -119,14 +172,3 @@ function salvaDeputado(newDeputado) {
         }
     });
 }
-
-request("http://www2.camara.leg.br/deputados/pesquisa", function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        const dom = new JSDOM(body);
-        var arraySize = dom.window.document.getElementById('deputado').length;
-        for (var i = 1; i < arraySize; i++) {
-            var temp = dom.window.document.getElementById('deputado')[i].value;
-            parseDeputadoInfo(temp.split("?")[1])
-        }
-    }
-});
